fe170425b09ca20a626daf53435587cfb74612c5
Riza Gumelar <rizza.gume@gmail.com>
2023-08-26 16:03:56 +0000

    changes model

--- models/departments.sql
+++ models/departments.sql
@@ -1,1 +1,12 @@
-SELECT * FROM `databasesuperstore.superstore.departments`
+{{
+    config(materialized='table')
+}}
+
+WITH x AS (
+    SELECT 
+        * 
+    FROM `databasesuperstore.superstore.departments`
+)
+SELECT
+    *, CURRENT_DATE('Asia/Jakarta') AS created_at
+FROM x
