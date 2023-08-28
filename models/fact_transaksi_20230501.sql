c2450ae003aa41f43ecfe8e3ec9c4203cde5db16
Riza Gumelar <rizza.gume@gmail.com>
2023-08-28 16:10:19 +0000

    add 1 model

---
+++ models/fact_transaksi_20230501.sql
@@ -0,0 +1,12 @@
+{{
+    config(materialized='table', description='merupakan data transaksi bulan mei')
+}}
+
+WITH x AS (
+SELECT 
+    * 
+FROM `data-engineering-riza.satu_data_master.fact_transaksi_20230501`)
+
+SELECT
+    *, CURRENT_DATE('Asia/Jakarta') AS created_at
+FROM x
