53298cabe177d0c84c37b2c703e105107fbbc4df
Riza Gumelar <rizza.gume@gmail.com>
2023-08-29 08:54:57 +0000

    changes model

---
+++ models/fact_transaksi_all.sql
@@ -0,0 +1,13 @@
+{{
+    config(materialized='table', alias='fact_transaksi_all', description='merupakan data transaksi bulan mei juni juli')
+}}
+
+WITH x AS (
+SELECT 
+    * 
+FROM data-engineering-riza.satu_data_master.fact_transaksi_all)
+
+SELECT
+    *, CURRENT_DATE('Asia/Jakarta') AS created_at
+FROM x
+WHERE x.transaksi_id IS NOT NULL
