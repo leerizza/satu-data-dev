bef97b370d17aafa7bea741f366a7ac9a5c7cd22
Riza Gumelar <rizza.gume@gmail.com>
2023-08-29 04:47:32 +0000

    modify 1 file

--- models/dim_barang.sql
+++ models/dim_barang.sql
@@ -3,7 +3,7 @@
 WITH x AS (
 SELECT 
     * 
-FROM `data-engineering-riza.satu_data_master.dim_barang`)
+FROM data-engineering-riza.satu_data_master.dim_barang)
 
 SELECT
     *, CURRENT_DATE('Asia/Jakarta') AS created_at
